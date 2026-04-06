d54f1aaf9573125753478dd2cf49ad9c3878dc12
JeronimoCabanelas <jeronimo.cabanela@gmail.com>
2026-04-06 10:55:49 -0500

    novo

---
+++ src/app/card/card.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  imports: [],
+  templateUrl: './card.html',
+  styleUrl: './card.css',
+})
+export class Card {
+
+  tituloPrincipal = 'why choose yourz';
+
+  diferenciais = [
+    {
+      icone: '/card/tickets.svg fill.png',
+      titulo: 'ultimate flexibility',
+      descricao: "You're in control, with  free cancellation..."
+    },
+    {
+      icone: '/card/hot-air-ballon.svg fill.png',
+      titulo: 'Memorable-experiences',
+      descricao: 'Browse and book yours and activities...'
+    },
+    {
+
+    }
+  ]
+}
